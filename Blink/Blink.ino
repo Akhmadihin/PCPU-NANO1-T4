34636c24ad74d35b0d4aef5bf003bdfd583c5e2d
Akhmadihin <akhmadihin@gmail.com>
2026-03-29 20:25:30 +0300

    Add files via upload

---
+++ Blink/Blink.ino
@@ -0,0 +1,14 @@
+void setup(){
+     pinMode(2, OUTPUT);
+     pinMode(3, OUTPUT);
+     pinMode(4, OUTPUT);
+     digitalWrite(2, HIGH);
+     digitalWrite(3, HIGH);
+     digitalWrite(4, HIGH);
+}
+void loop(){
+     digitalWrite(4, HIGH);
+     delay(75);
+     digitalWrite(4, LOW);
+     delay(75);
+}
